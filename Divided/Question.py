9f4f5ad0d61c0187068a29bd32e4d8f898916634
Jia Billones <95361615+jiabillones@users.noreply.github.com>
2024-08-23 22:43:45 +0800

    Update Question.py
    Comment abstract class, display_question, check_answer

--- Divided/Question.py
+++ Divided/Question.py
@@ -1,14 +1,24 @@
 from abc import ABC, abstractmethod
 
 class Question(ABC):
+    '''
+    An abstract base class that represents the general quiz questions.
+    '''
+    
     def __init__(self, question_text, correct_answer, category):
+    """
+    Initializes the Question with its text (statement), the correct answer, and category.
+    """
         self.question_text = question_text
         self.correct_answer = correct_answer
         self.category = category
 
     @abstractmethod
     def display_question(self):
-        pass
+    """
+    Abstract method to define how the question is displayed to the user
+    """
+        pass    # Empty method for the abstract
 
     @abstractmethod
     def check_answer(self, answer):
