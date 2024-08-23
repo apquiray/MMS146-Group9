bca964010d0c58422b96d47b167b0394c53f102c
ashley <95933245+gevajashley@users.noreply.github.com>
2024-08-23 22:28:36 +0800

    Update TrueFalseQuestion.py
    added docstring comments and edited the method check_answer

--- Divided/TrueFalseQuestion.py
+++ Divided/TrueFalseQuestion.py
@@ -1,8 +1,22 @@
 from Question import Question
 
 class TrueFalseQuestion(Question):
+    '''
+    A class for true/false questions
+    '''
     def display_question(self):
+    '''
+    Displays the question with T/F options
+    '''
         print(f"\n{self.question_text} (True/False)")
     
     def check_answer(self, answer):
-        return answer.lower() == self.correct_answer.lower()
+    '''
+    Checks if the answer is correct
+    '''
+        if answer.upper() == "T":
+            return self.correct_answer.lower() == "true"
+        elif answer.upper() == "F":
+            return self.correct_answer.lower() == "false"
+        else:
+            return False
