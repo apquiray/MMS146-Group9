5631ea4ef277abb5b5fd9ad1e2b8d97ffe51a009
pmichelledean <95409961+pmichelledean@users.noreply.github.com>
2024-08-25 03:06:36 +0800

    Update Question.py

--- Divided/Question.py
+++ Divided/Question.py
@@ -21,7 +21,7 @@ class Question(ABC):
         pass    # Empty method for the abstract
 
     """
-    Abstract method that checks if the user's answer is correct
+    The abstract method checks if the user's answer is correct.
         
     This is done by comparing the user's answer to the correct answer specified in the code. 
     This method has different implementations per subclass. 
@@ -32,17 +32,17 @@ class Question(ABC):
         pass 
 
     """
-    A dictionary containing the attributes of the object question
-    Making it easier to store or retrieve information on the questions
+    This method creates a dictionary containing the attributes of the object question.
+    Making it easier to store or retrieve information on the questions.
     """    
     def to_dict(self):
         return {
-            # The question itself
+            # Stores text question itself
             "question_text": self.question_text, 
-             # The correct answer  
+            # Stores the defined correct answer  
             "correct_answer": self.correct_answer,  
-            # The topic category
+            # Stores the defined topic category
             "category": self.category,   
-            # The type of question, multiple choice or true/false.
+            # Defines the type of question, multiple choice or true/false
             "type": self.__class__.__name__            
         }
