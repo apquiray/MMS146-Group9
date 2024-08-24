842894cc2d8c0ff817fe1b402ca96b6cb2bc738e
ashley <95933245+gevajashley@users.noreply.github.com>
2024-08-25 01:46:22 +0800

    Update TrueFalseQuestion.py
    edited the inline comments and docstrings

--- Divided/TrueFalseQuestion.py
+++ Divided/TrueFalseQuestion.py
@@ -2,26 +2,23 @@ from Question import Question
 
 class TrueFalseQuestion(Question):
     '''
-    This class handles True or False type questions for the reviewer.
-    This class inherits from the Question class and contains the methods 
-    display_question and check_answer.
+    This class is inherited from the Question class
+    and handles True or False type questions for the reviewer.
     '''
+
+    # This method displays the question prompt
     def display_question(self):
-    '''
-    This method displays the current questions to the user with the options 
-    to answer with 'True' or 'False' and prompts the user to choose between
-    T for True and F for false.
-    '''
         print(f"\n{self.question_text} (T for TRUE, F for FALSE)")
-    
+
+    # This method validates the users answer
+    # It also checks if the users answer matches the expected response
     def check_answer(self, answer):
-    '''
-    This method validates the user's answer. True if the user's answer matches
-    the correct answer, otherwise, False if the answer provided is invalid.
-    '''
+        # Checks if answer is True
         if answer.upper() == "T":
             return self.correct_answer.lower() == "true"
+        # Checks if answer is False
         elif answer.upper() == "F":
             return self.correct_answer.lower() == "false"
+        # Invalid response
         else:
             return False
