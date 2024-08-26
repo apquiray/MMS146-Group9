fe7b1f04cb8585500672479d33d9730dc45037f4
Jia Billones <95361615+jiabillones@users.noreply.github.com>
2024-08-26 21:00:49 +0800

    Update MultipleChoiceQuestion.py
    Added docstrings to class MultipleChoiceQuestion(Question), def display_question(self), def check_answer(self, answer), def to_dict(self)

--- Divided/MultipleChoiceQuestion.py
+++ Divided/MultipleChoiceQuestion.py
@@ -1,16 +1,26 @@
 from Question import Question
 
+    """
+    The class MultipleChoiceQuestion is a subclass and inherits the Question superclass.
+    """
 class MultipleChoiceQuestion(Question):
     def __init__(self, question_text, options, correct_answer, category):
         super().__init__(question_text, correct_answer, category)
         self.options = options
 
+    """
+    This method displays the question to users.
+    This is followed by options ranging from A, B, C, etc. 
+    """
     def display_question(self):
         print(f"\n{self.question_text}")
         for i, option in enumerate(self.options):
             letter = chr(65 + i)
             print(f"{letter}: {option}")
-    
+        
+    """
+    This method checks the answer to see whether it is acceptable on the conditions given.
+    """
     def check_answer(self, answer):
         try:
             index = ord(answer.upper()) - 65
@@ -18,7 +28,10 @@ class MultipleChoiceQuestion(Question):
             return selected_option == self.correct_answer
         except (ValueError, IndexError):
             return False
-
+    """
+    This method creates a dictionary containing the attributes of the MultipleChoiceQuestion.
+    Making it easier to store or retrieve information on the MultipleChoicequestions.
+    """
     def to_dict(self):
         data = super().to_dict()
         data["options"] = self.options
