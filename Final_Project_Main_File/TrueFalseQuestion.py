7af849361b7730e85a6c7f5699da9f4cd7542f44
jassllantada <95325016+jassllantada@users.noreply.github.com>
2024-08-29 21:39:13 +0800

    Update TrueFalseQuestion.py

--- Final_Project_Main_File/TrueFalseQuestion.py
+++ Final_Project_Main_File/TrueFalseQuestion.py
@@ -10,15 +10,15 @@ class TrueFalseQuestion(Question):
     def display_question(self):
         print(f"\n{self.question_text} (T for True, F for False)")
     
-    # This method validates the users answer
-    # It also checks if the users answer matches the expected response
+    # This method validates the user's answer
+    # It also checks if the user's answer matches the expected response
     def check_answer(self, answer):
-        # Checks if answer is True
+        # Checks if the answer is True
         if answer.upper() == "T":
             return self.correct_answer.lower() == "true"
-        # Checks if answer is False
+        # Checks if the answer is False
         elif answer.upper() == "F":
             return self.correct_answer.lower() == "false"
-        # This will make the answer wrong if the user did not respond.
+        # This will make the answer wrong if the user does not respond.
         else:
             return False
